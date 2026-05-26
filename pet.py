78b634cd3b642ecbe442e64aadc0609ec5221d17
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 16:13:26 +0800

    added a default value for all attributes

--- pet.py
+++ pet.py
@@ -1,5 +1,5 @@
 class Pet:
-    def __init__(self, name, animal_type, age):
+    def __init__(self, name="", animal_type="", age=0):
         self.__name = name
         self.__animal_type = animal_type
         self.__age = age
