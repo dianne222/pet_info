4c9b039d059bd5ed50467b94427c95b66865bf27
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 16:06:23 +0800

    added a get pet info method

--- get_pet_info.py
+++ get_pet_info.py
@@ -1,4 +1,8 @@
 from pet import Pet
 
+pet1 = Pet()
+
 def get_pet_info():
-    pet1 = Pet()
+    name = input("Enter your's pet name:")
+    animal_type = input("Enter your animal's type:")
+    age = int(input("Enter your age:"))
