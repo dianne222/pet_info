99e92ff698cc69208bc5c1a539b70b0b5305b7c7
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 16:08:19 +0800

    added a store pet info method

--- get_pet_info.py
+++ get_pet_info.py
@@ -6,3 +6,8 @@ def get_pet_info():
     name = input("Enter your's pet name:")
     animal_type = input("Enter your animal's type:")
     age = int(input("Enter your age:"))
+
+def store_pet_info():
+    pet1.set_name(name)
+    pet1.set_animal_type(animal_type)
+    pet1.set_age(age)
