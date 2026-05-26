bf559a5bd9458a1a577b2d3238220ec67985f5a6
Dianne <crisdiannecasino2@gmail.com>
2026-05-26 16:11:49 +0800

    added attributes in store method

--- get_pet_info.py
+++ get_pet_info.py
@@ -7,7 +7,7 @@ def get_pet_info():
     animal_type = input("Enter your animal's type:")
     age = int(input("Enter your age:"))
 
-def store_pet_info():
+def store_pet_info(name, animal_type, age):
     pet1.set_name(name)
     pet1.set_animal_type(animal_type)
     pet1.set_age(age)
@@ -18,3 +18,7 @@ def display_pet_info():
     print("Animal Type:", pet1.get_animal_type())
     print("Age:", pet1.get_age())
     print("")
+
+get_pet_info()
+store_pet_info()
+display_pet_info()
